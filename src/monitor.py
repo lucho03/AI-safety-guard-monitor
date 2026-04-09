da156ec5fb537af634951309c35e2b9ee5a51cf9
lucho03 <l.valentinov.ivanov@gmail.com>
2026-04-09 10:42:02 +0300

    Clean up the monitor functionality

--- src/monitor.py
+++ src/monitor.py
@@ -1,7 +1,9 @@
 import json
 import os
-import matplotlib.pyplot as plt
 import paho.mqtt.client as mqtt
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from queue import Queue
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,46 +11,61 @@ load_dotenv()
 TOPIC_DEBIT = os.getenv("TOPIC_DEBIT")
 TOPIC_QUANTITY = os.getenv("TOPIC_QUANTITY")
 
-client = mqtt.Client()
-client.username_pw_set(os.getenv("MONITOR_USERNAME"), os.getenv("MONITOR_PASSWORD"))
-client.tls_set(
-    ca_certs=os.getenv("CA_CRT"),
-    certfile=os.getenv("MONITOR_CRT"),
-    keyfile=os.getenv("MONITOR_KEY")
-)
-client.connect(os.getenv("BROKER"), int(os.getenv("PORT")))
+message_queue = Queue()
 
-flow_data = []
-level_data = []
+# ---------- MQTT ----------
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT")
+    client.subscribe(TOPIC_DEBIT)
+    client.subscribe(TOPIC_QUANTITY)
 
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload)
 
     if msg.topic == TOPIC_DEBIT:
-        flow_data.append(data["flow_rate"])
-        print(f"Received flow rate: {flow_data[-1]}")
+        message_queue.put(("flow_update", data["flow_rate"]))
 
-    elif msg.topic == TOPIC_QUANTITY:
-        level_data.append(data["level"])
-        print(f"Received tank level: {level_data[-1]}")
+    if msg.topic == TOPIC_QUANTITY:
+        message_queue.put(("level_update", data["level"]))
 
-client.on_message = on_message
-client.subscribe(TOPIC_DEBIT)
-client.subscribe(TOPIC_QUANTITY)
+def create_mqtt():
+    client = mqtt.Client()
+    client.username_pw_set(os.getenv("MONITOR_USERNAME"), os.getenv("MONITOR_PASSWORD"))
+    client.tls_set(
+        ca_certs=os.getenv("CA_CRT"),
+        certfile=os.getenv("MONITOR_CRT"),
+        keyfile=os.getenv("MONITOR_KEY")
+    )
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(os.getenv("BROKER"), int(os.getenv("PORT")))
+    return client
 
-plt.ion()
+mqtt_client = create_mqtt()
+mqtt_client.loop_start()
 
-while True:
-    client.loop()
+# ---------- Flask ----------
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-    plt.clf()
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
+    print("Clients:", socketio.server.manager.rooms)
 
-    plt.subplot(2,1,1)
-    plt.title("Flow Rate")
-    plt.plot(flow_data[-50:])
+def socket_worker():
+    while True:
+        event, value = message_queue.get()
+        print("Sending to control panel:", event, value)
+        socketio.emit(event, {"value": value}, namespace="/")
+        socketio.sleep(0)
 
-    plt.subplot(2,1,2)
-    plt.title("Tank Level")
-    plt.plot(level_data[-50:])
+socketio.start_background_task(socket_worker)
 
-    plt.pause(1)
+@app.route("/")
+def index():
+    return render_template("dashboard.html")
+
+# ---------- Run ----------
+if __name__ == "__main__":
+    socketio.run(app, host="127.0.0.1", port=5000)
